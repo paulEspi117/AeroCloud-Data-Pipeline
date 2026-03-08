c8d77f329ae4c7aaecd700e5b99548c3cb9f7bd2
Paul Espinasse <126152491+paulEspi117@users.noreply.github.com>
2026-03-08 21:52:43 +0100

    Derniers correctifs

--- src/analytics/2026-03-03 5_30pm.sql
+++ src/analytics/2026-03-03 5_30pm.sql
@@ -18,19 +18,18 @@ CREATE OR REPLACE TABLE FLIGHTS_ANALYTICS (
     INGESTION_TIMESTAMP TIMESTAMP
 );
 
--- 3. Configuration du format de fichier (Databricks a écrit du Parquet sous le capot de Delta)
+-- 3. Configuration du format de fichier 
 CREATE OR REPLACE FILE FORMAT PARQUET_FORMAT
     TYPE = PARQUET
     COMPRESSION = SNAPPY;
 
--- 4. Création du Stage Externe pointant vers ton Azure Data Lake
--- RAPPEL : Retire le "?" au début de ton jeton SAS si Azure l'a mis.
+-- 4. Création du Stage Externe pointant vers le Azure Data Lake
 CREATE OR REPLACE STAGE AZURE_SILVER_STAGE
     URL = 'azure://aerocloudstorage.blob.core.windows.net/silver/flights/'
     CREDENTIALS = (AZURE_SAS_TOKEN = 'sp=rl&st=2026-03-03T15:55:38Z&se=2026-03-08T00:10:38Z&spr=https&sv=2024-11-04&sr=c&sig=jAjKlA9BVyqpKGTxiLuXkWbrrDRLCjQR3sZ7PHJ9sUQ%3D')
     FILE_FORMAT = PARQUET_FORMAT;
 
--- 5. Test de connexion (Si ça marche, tu verras la liste de tes fichiers .parquet !)
+-- 5. Test de connexion 
 LIST @AZURE_SILVER_STAGE;
 
 -- 6. Chargement des données dans la table avec gestion des erreurs
@@ -39,7 +38,7 @@ FROM @AZURE_SILVER_STAGE
 MATCH_BY_COLUMN_NAME = CASE_INSENSITIVE
 ON_ERROR = 'CONTINUE';
 
--- 7. Vérification finale !
+-- 7. Vérification 
 SELECT * FROM FLIGHTS_ANALYTICS LIMIT 100;
 
 SELECT
--- src/ingestion/function_app.py
+++ src/ingestion/function_app.py
@@ -16,8 +16,7 @@ def fetch_opensky_data(myTimer: func.TimerRequest) -> None:
 
     logging.info('Exécution de la fonction Azure pour récupérer les données OpenSky.')
 
-    # 1. Appel à l'API OpenSky (Bounding box pour la France/Occitanie pour limiter la taille)
-    # lamin, lomin, lamax, lomax
+    # Appel à l'API OpenSky 
     url = "https://opensky-network.org/api/states/all?lamin=42.0&lomin=-5.0&lamax=51.0&lomax=8.0"
     
     try:
@@ -25,19 +24,19 @@ def fetch_opensky_data(myTimer: func.TimerRequest) -> None:
         response.raise_for_status()
         flight_data = response.json()
         
-        # 2. Préparation du nom de fichier avec un timestamp
+        # nom de fichier avec un timestamp
         timestamp = datetime.utcnow().strftime('%Y%m%d_%H%M%S')
         file_name = f"flights_france_{timestamp}.json"
         
-        # 3. Connexion à Azure Data Lake Storage (ADLS Gen2)
-        # La chaîne de connexion doit être dans les variables d'environnement (ou local.settings.json)
+        #  Connexion  Azure Data Lake Storage
+        # chaîne de connexion dans local.settings.json
         connection_string = os.environ["ADLS_CONNECTION_STRING"]
         service_client = DataLakeServiceClient.from_connection_string(connection_string)
         
-        # On cible le conteneur "bronze" (données brutes)
+        # conteneur bronze
         file_system_client = service_client.get_file_system_client(file_system="bronze")
         
-        # Création du client pour le fichier et upload
+        # Création client pour le fichier et upload
         file_client = file_system_client.get_file_client(f"opensky/{datetime.utcnow().strftime('%Y/%m/%d')}/{file_name}")
         
         # Upload des données JSON converties en string
